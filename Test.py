50a7864381f6058c7356bdc83298654d4d3fa396
Anthony <anthonyyylu24@gmail.com>
2023-05-15 21:20:43 -0500

    Hello

--- Test.py
+++ Test.py
@@ -1,2 +1,7 @@
-x = "abcededhi"
-print(x[2])
+num = "1234567892222222987654321"
+def pandigital(n):
+    num = str(n)
+    if "1" in num and "2" in num and "3" in num and "4" in num and "5" in num and "6" in num and "7" in num and "8" in num and "9" in num:
+        return True
+    return False 
+print(pandigital(num[0:9]), pandigital(num[len(num):len(num) - 10: -1]))
